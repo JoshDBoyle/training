2a7a80a22e06034985d858fa166b0e2f3e96acb3
mocavada <marc@awesomewebsite.ca>
2021-12-08 18:29:36 -0500

    done D3

--- core/src/main/java/com/accenture/core/models/HeaderModel.java
+++ core/src/main/java/com/accenture/core/models/HeaderModel.java
@@ -1,30 +1,30 @@
-package com.accenture.core.models;
-
-
-import com.adobe.cq.wcm.core.components.commons.link.Link;
-import com.adobe.cq.wcm.core.components.models.ListItem;
-import com.adobe.cq.wcm.core.components.models.Navigation;
-import com.adobe.cq.wcm.core.components.models.Teaser;
-import com.adobe.cq.wcm.core.components.models.datalayer.ComponentData;
-import org.apache.sling.api.SlingHttpServletRequest;
-import org.apache.sling.api.resource.Resource;
-import org.apache.sling.models.annotations.DefaultInjectionStrategy;
-import org.apache.sling.models.annotations.Model;
-import org.apache.sling.models.annotations.Via;
-import org.apache.sling.models.annotations.injectorspecific.Self;
-import org.apache.sling.models.annotations.via.ResourceSuperType;
-
-import java.util.List;
-
-@Model(adaptables = SlingHttpServletRequest.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL, adapters = Navigation.class, resourceType = "training/components/header")
-public class HeaderModel implements Navigation {
-
-
-    @Self
-    @Via(type = ResourceSuperType.class)
-    private Navigation navigation;
-
-
+//package com.accenture.core.models;
+//
+//
+//import com.adobe.cq.wcm.core.components.commons.link.Link;
+//import com.adobe.cq.wcm.core.components.models.ListItem;
+//import com.adobe.cq.wcm.core.components.models.Navigation;
+//import com.adobe.cq.wcm.core.components.models.Teaser;
+//import com.adobe.cq.wcm.core.components.models.datalayer.ComponentData;
+//import org.apache.sling.api.SlingHttpServletRequest;
+//import org.apache.sling.api.resource.Resource;
+//import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+//import org.apache.sling.models.annotations.Model;
+//import org.apache.sling.models.annotations.Via;
+//import org.apache.sling.models.annotations.injectorspecific.Self;
+//import org.apache.sling.models.annotations.via.ResourceSuperType;
+//
+//import java.util.List;
+//
+//@Model(adaptables = SlingHttpServletRequest.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL, adapters = Navigation.class, resourceType = "training/components/header")
+//public class HeaderModel implements Navigation {
+//
+//
+//    @Self
+//    @Via(type = ResourceSuperType.class)
+//    private Navigation navigation;
+//
+//
 //    @Override
 //    public String getDescription() {
 //        return teaser.getDescription() + "\nDisclaimer: I'm an AEM Ninja";
@@ -49,9 +49,9 @@ public class HeaderModel implements Navigation {
 //    public String getLinkURL() {
 //        return teaser.getLinkURL();
 //    }
-
-
-
-
-
-}
+//
+//
+//
+//
+//
+//}
